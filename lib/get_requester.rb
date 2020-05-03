7ffca5fcd8547af787a581bcb242c0879377eb20
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-03 22:09:38 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,30 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+ 
+class GetRequester
+  
+attr_accessor :url 
+
+  def initialize(string)
+    @url = "string"
+  end
+ 
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  
+  def parse_json
+  klass = JSON.parse(self.get_response_body)
+  klass.collect do |x|
+    x[y]  
+  end
+end
+ 
+end
+
+
+
